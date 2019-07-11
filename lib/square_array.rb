89cc9c1bc44bb51fc89d4bb9c38d87db80a38476
Chris Hodge <chrishodge87@gmail.com>
2019-07-11 14:47:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -9,18 +9,18 @@ while counter < array.length
     counter += 1
   end
 
-  
+
 end
 
 
 
 
 numbers = [1,2,3]
- 
+
 square_array(numbers)
 # => [1,4,9])
- 
+
 new_numbers = [9,10,16,25]
- 
+
 square_array(new_numbers)
 # => [81,100,256,625]
