99fc0a51bdb174e6782807d6411be2b5b21d3c25
Chris Hodge <chrishodge87@gmail.com>
2019-07-11 15:05:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,7 +5,7 @@ counter = 0
 new_array = []
 while counter < array.length do
   item = array[counter]
-    new_array[counter] = Math.sqrt(item)
+    new_array[counter] = item**2
     counter += 1
 
   end
